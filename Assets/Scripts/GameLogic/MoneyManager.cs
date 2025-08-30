6a522a998341728fd439a97dfb8f37c35ee40382
Vylax <vylaxez@gmail.com>
2025-08-30 14:59:44 +0200

    removed Saving system

---
+++ Assets/Scripts/GameLogic/MoneyManager.cs
@@ -0,0 +1,23 @@
+public static class MoneyManager {
+
+    private static int money = 0;
+
+    public static int Money => money;
+
+    public static void AddMoney(int amount) {
+        money += amount;
+    }
+
+    public static void RemoveMoney(int amount) {
+        money -= amount;
+        if (money <= 0) {
+            money = 0;
+            // Trigger Bankrupt Event
+            GameManager.Instance.Bankrupt();
+        }
+    }
+    
+    public static void ResetMoney() {
+        money = 0;
+    }
+}
